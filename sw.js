05f85884854ae533e81463e76edbb7cbdb14308b
Dr.IM <islamcerberev@gmail.com>
2026-05-26 00:06:16 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'messenger-cache-v1';
+const urlsToCache = [
+  '/my-chat/',
+  '/my-chat/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
